6bb6f375601b1d06a46dda718046982ea87c3cb2
huanyuejue <w1376158303@gmail.com>
2026-09-12 10:55:11 +0800

    优化设置页功能项布局和显示问题

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -507,7 +507,9 @@ public partial class MainWindow : Window
         }
         if (tag != "Settings")
         {
+            // 两个测速结果面板的可见性都绑在集合 Count 上，切出设置页统一清空收起
             _settingsViewModel.SpeedTestResults.Clear();
+            _settingsViewModel.MirrorSpeedTestResults.Clear();
             _settingsViewModel.StatusMessage = "";
         }
         var prevIndex = Array.IndexOf(_navOrder, _prevTag);
